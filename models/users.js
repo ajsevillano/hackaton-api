ff3b911c8323a18320d66cd7009f0ef3a5f56804
Sevi <correosevi@gmail.com>
2022-01-23 20:33:06 +0000

    Create response handler function

--- models/users.js
+++ models/users.js
@@ -7,5 +7,13 @@ export function getAllUsers() {
     { id: 1, name: 'Mike' },
     { id: 2, name: 'Andrew' },
   ];
-  return data;
+  return responseHandler(true, data);
+}
+
+//RESPONSE HANDLER
+function responseHandler(status, statusMsg) {
+  return {
+    success: status,
+    payload: statusMsg,
+  };
 }
--- routes/users.js
+++ routes/users.js
@@ -5,10 +5,7 @@ import { getAllUsers } from '../models/users.js';
 // GET ALL USERS
 router.get(`/`, async function (req, res) {
   const result = await getAllUsers();
-  res.json({
-    success: true,
-    payload: result,
-  });
+  res.json(result);
 });
 
 export default router;
