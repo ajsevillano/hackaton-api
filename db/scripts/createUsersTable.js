5332c6e38e43392815594cd68189aae4235d3daf
Sevi <correosevi@gmail.com>
2022-01-23 20:50:40 +0000

    Create the script to create the users table

---
+++ db/scripts/createUsersTable.js
@@ -0,0 +1,18 @@
+import query from '../index.js';
+
+const userQuery = `CREATE TABLE IF NOT EXISTS users(
+  id SERIAL PRIMARY KEY,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  gender TEXT,
+  ip_address TEXT,
+  timestamp TIMESTAMPTZ NOT NULL DEFAULT now()
+  )`;
+
+async function createUserTable() {
+  const response = await query(userQuery);
+  console.log(response);
+}
+
+createUserTable();
