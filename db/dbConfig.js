eebeee33b76f62c219d8565163ac246962c04b23
Sevi <correosevi@gmail.com>
2022-01-23 20:35:14 +0000

    Create DB index and config files

---
+++ db/dbConfig.js
@@ -0,0 +1,9 @@
+const dbConfig = {
+  user: process.env.DBUSER,
+  host: process.env.DBHOST,
+  database: process.env.DBNAME,
+  password: process.env.DBPASSWORD,
+  port: process.env.DBPORT,
+};
+
+export default dbConfig;
