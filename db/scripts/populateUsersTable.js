807e0ae8031ce2f18593106b9e2e7b08265588dd
Sevi <correosevi@gmail.com>
2022-01-24 10:24:49 +0000

    Create script to populate the users table

---
+++ db/scripts/populateUsersTable.js
@@ -0,0 +1,24 @@
+import query from '../index.js';
+import { users } from '../mockdata.js';
+
+async function populateUsersTable() {
+  for (let index = 0; index < users.length; index++) {
+    const { first_name, last_name, email, gender, ip_address } = users[index];
+
+    const timestamp = 'now()';
+
+    //SQL to populate the table.
+    const sqlString = `INSERT into users (first_name,last_name,email,gender,ip_address,timestamp) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *`;
+    const response = await query(sqlString, [
+      first_name,
+      last_name,
+      email,
+      gender,
+      ip_address,
+      timestamp,
+    ]);
+    console.log(response);
+  }
+}
+
+populateUsersTable();
