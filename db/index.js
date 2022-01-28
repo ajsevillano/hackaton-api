41b0b4a21ffe4ed51133219a833ddadec1219813
Sevi <correosevi@gmail.com>
2022-01-28 12:15:35 +0000

    push

--- db/index.js
+++ db/index.js
@@ -2,11 +2,7 @@ import pg from 'pg';
 import dbConfig from './dbConfig.js';
 
 const pool = new pg.Pool({
-  user: dbConfig.user,
-  host: dbConfig.host,
-  database: dbConfig.database,
-  password: dbConfig.password,
-  port: dbConfig.port,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
 
