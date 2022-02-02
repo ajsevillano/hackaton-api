0307a65c7638f24607d36ea69d7b4f33697b72aa
Sevi <correosevi@gmail.com>
2022-02-02 20:13:09 +0000

    Create separate conf for dev & prod in db script

--- db/index.js
+++ db/index.js
@@ -1,15 +1,25 @@
 import pg from 'pg';
 import dbConfig from './dbConfig.js';
 
-const pool = new pg.Pool({
-  // user: dbConfig.user,
-  // host: dbConfig.host,
-  // database: dbConfig.database,
-  // password: dbConfig.password,
-  // port: dbConfig.port,
+//Development DB configuration
+const localConfigObject = {
+  user: dbConfig.user,
+  host: dbConfig.host,
+  database: dbConfig.database,
+  password: dbConfig.password,
+  port: dbConfig.port,
+  ssl: { rejectUnauthorized: false },
+};
+
+//Production DB configuration
+const productionConfigObject = {
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
-});
+};
+
+const pool = new pg.Pool(
+  process.env.PROJECT_ENV === 'dev' ? localConfigObject : productionConfigObject
+);
 
 export default function query(text, params) {
   return pool.query(text, params);
